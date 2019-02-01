03f62edb3d1e53f530ca3b417956452aa4f958f4
Alejandro Loza <alejandrolg@MacBook-Pro-de-Alejandro.local>
2019-01-31 21:03:13 -0600

    First Commit

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+var v1 = "esto es Swift"
+let c1 = 4
+var cadenaString = String()
+var cadena = "Hola mundo " + v1
+print("El contenido es: \(cadena)")
+print("El valor de constante es: \(c1)")
